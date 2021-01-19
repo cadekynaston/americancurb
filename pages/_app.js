351faa4a35e176081403dda8f60247d8d5960436
Cade Kynaston <cadekynaston@gmail.com>
2021-01-19 16:04:07 -0700

    add GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,25 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import { ThemeProvider } from 'theme-ui';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 import theme from '../src/utils/theme';
 import 'fontsource-poppins';
 import '../node_modules/normalize.css/normalize.css';
 
-const App = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: 'G-HZ5LJV12BL',
+    });
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
 
 export default App;
