88db727eea335aeb9d6a6b8d4dc9f9801f969073
Cade Kynaston <cadekynaston@gmail.com>
2021-01-20 10:52:32 -0700

    remove custom pageview function

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,15 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import { ThemeProvider } from 'theme-ui';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import * as gtag from '../lib/gtag';
 
 import theme from '../src/utils/theme';
 import 'fontsource-poppins';
 import '../node_modules/normalize.css/normalize.css';
 
-const App = ({ Component, pageProps }) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
-};
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
 
 export default App;
