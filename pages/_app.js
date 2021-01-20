a4da9672f1e8c84a09b0deb7f5b56fa5c12ec3d5
Cade Kynaston <cadekynaston@gmail.com>
2021-01-20 10:42:56 -0700

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,26 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import { ThemeProvider } from 'theme-ui';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
+import * as gtag from '../lib/gtag';
 
 import theme from '../src/utils/theme';
 import 'fontsource-poppins';
 import '../node_modules/normalize.css/normalize.css';
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+
   useEffect(() => {
-    TagManager.initialize({
-      gtmId: 'UA-137604730-7',
-    });
-  }, []);
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <ThemeProvider theme={theme}>
